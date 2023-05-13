8279c1483991606e6784f69c77b4af91ba18d564
Julian Röckl <58952591+Tidestorm@users.noreply.github.com>
2023-05-13 18:31:39 +0200

    Implemented empty classes for different databases

---
+++ Assets/Scripts/FirestoreDatabaseManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirestoreDatabaseManager : DatabaseFunctions
+{
+    //TODO: Logik für FirestoreDatabase implementieren und aus FirebaseManager aufrufen, sobald die Checkbox aktiviert ist,
+    // dass FirestoreDatabase aktiviert ist (in FirebaseManager durch Checkbox validieren ob FirestoreDatabaseManager oder RealtimeDatabaseManager verwendet werden soll.)
+
+    public void LoadUserData()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/RealtimeDatabaseManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealtimeDatabaseManager : DatabaseFunctions
+{
+    //TODO: logik aus FirebaseManager bzgl RealtimeDatabase entfernen und hier aufrufen.
+
+    public void LoadUserData()
+    {
+        throw new System.NotImplementedException();
+    }
+}
